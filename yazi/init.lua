02e312e04fcbe28369e4fa873e18d335dfb0a978
HC3560 <hc3560@qq.com>
2025-03-02 14:17:57 +0800

    Yazi: Add plugin 'yamb.yazi'

--- yazi/init.lua
+++ yazi/init.lua
@@ -26,6 +26,23 @@ require("yaziline"):setup({
 -- HC-Plugin: full-border.yazi
 require("full-border"):setup()
 
+-- HC-Plugin: yamb.yazi
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+require("yamb"):setup({
+	-- Optional, the path ending with path seperator represents folder.
+	bookmarks = bookmarks,
+	-- Optional, recieve notification everytime you jump.
+	jump_notify = true,
+	-- Optional, the cli of fzf.
+	cli = "fzf",
+	-- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+	keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	-- Optional, the path of bookmarks
+	path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
+		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+})
+
 -- 在状态栏中显示符号链接：https://yazi-rs.github.io/docs/tips/#symlink-in-status
 Status:children_add(function(self)
 	local h = self._current.hovered
